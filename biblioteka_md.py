697b1858df594cf7ed26d7e585cd8d766b322dd8
dainireR <101051579+dainireR@users.noreply.github.com>
2026-03-24 11:36:46 +0200

    Ieteikumi_biblioteka_md.py
    Apskaties ieteikumus

--- biblioteka_md.py
+++ biblioteka_md.py
@@ -1,6 +1,6 @@
 import pyjokes
-
-print(pyjokes.get_joke())
+joks=pyjokes.get_joke()
+print(joks)
 
 import pyfiglet
 vards=pyfiglet.figlet_format("Valērija", font="Comic Sans MS")
@@ -11,3 +11,10 @@ import cowsay
 cowsay.cow(pyjokes.get_joke())
 
 print(cowsay.get_output_string('cow', pyjokes.get_joke()))
+
+
+#4.uzd
+
+#pārveido ievadīto joku ar pyfiglet līdzīgi kā 6.rindā, tikai vārda vietā ieraksti joks
+
+##pārveido 18.rindas mainīgo, kuru pārveidoji ar pyfiglet, ar cow say.  var arī 1 rindā
